e30bd4df17de7f32108736eb669dd35a4e81a8dc
0x4A6F <0x4A6F@users.noreply.github.com>
2023-10-26 10:25:41 +0200

    atuin: 16.0.0 -> 17.0.0
    - Release: https://github.com/atuinsh/atuin/releases/tag/v17.0.0
    - Changes: https://github.com/atuinsh/atuin/compare/v16.0.0...v17.0.0

--- pkgs/tools/misc/atuin/default.nix
+++ pkgs/tools/misc/atuin/default.nix
@@ -11,20 +11,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "atuin";
-  version = "16.0.0";
+  version = "17.0.0";
 
   src = fetchFromGitHub {
     owner = "atuinsh";
     repo = "atuin";
     rev = "v${version}";
-    hash = "sha256-Kh6aaWYV+ZG7Asvw5JdGsV+nxD+xvvQab5wLIedcQcQ=";
+    hash = "sha256-eGukHoh1S2CtehapSRUM3+cnVvTpslwvDCSXlrnC4+4=";
   };
 
   # TODO: unify this to one hash because updater do not support this
   cargoHash =
     if stdenv.isLinux
-    then "sha256-Ami88ScGj58jCCat4MMDvjZtV5WglmrlggpQfo+LPjs="
-    else "sha256-HQMZ9w1C6go16XGrPNniQZliIQ/5yAp2w/uUwAOQTM0=";
+    then "sha256-KlfNOZ5xDID+x0GohbLv+SJlPQRK6C5YzbbFnKvm2ng="
+    else "sha256-8OarzrMc7EiHEDDbs2XiggivebQ68yufnmhXsf6MBwM=";
 
   nativeBuildInputs = [ installShellFiles ];
 
@@ -44,6 +44,8 @@ rustPlatform.buildRustPackage rec {
   checkFlags = [
     # tries to make a network access
     "--skip=registration"
+    # No such file or directory (os error 2)
+    "--skip=sync"
   ];
 
   meta = with lib; {
